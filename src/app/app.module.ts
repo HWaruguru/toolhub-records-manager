23039f94c57ebe6a6726494f7bda25fd3966070f
Hannah Waruguru <hannah.waruguru8@gmail.com>
2022-10-25 19:33:23 +0300

    components: Add navbar and empty components
    Create a navbar and it's menus
    Add the leaderboard, home and records dashboard with routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { LeaderboardComponent } from './components/leaderboard/leaderboard.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    LeaderboardComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
